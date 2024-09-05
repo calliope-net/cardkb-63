34d1d98da4f56068555b96b11dd0ec6fb81e9df9
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-05 20:11:40 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    buffer = pins.pins_i2cReadBuffer(i2cAdresse, 1)
+    basic.showNumber(pins.buffer_getUint8(buffer, 0))
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showString(CardKB.readLetter(characterFormat.ascii))
+})
+let buffer: Buffer = null
+let i2cAdresse = 0
+i2cAdresse = pins.pins_i2cAdressen(pins.ei2cAdressen.CardKB_x5F)
